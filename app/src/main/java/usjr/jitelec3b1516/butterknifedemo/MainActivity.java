268ec3d4cf45ffa240d5fd63f5f033ef64ec7ead
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2015-07-01 17:17:01 +0800

    Added string resources. Added simple xml view layout. Implemented functionality on adding simple string item to list. Implemented functionality on handling list item click.

--- app/src/main/java/usjr/jitelec3b1516/butterknifedemo/MainActivity.java
+++ app/src/main/java/usjr/jitelec3b1516/butterknifedemo/MainActivity.java
@@ -1,37 +1,57 @@
 package usjr.jitelec3b1516.butterknifedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnItemClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.etItem)
+    EditText mEtItem;
+
+    @Bind(R.id.listItem)
+    ListView mListItem;
+
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                new ArrayList<String>());
+        mListItem.setAdapter(mAdapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    @OnClick(R.id.btnAddItem)
+    public void addItem() {
+        String item = mEtItem.getText().toString();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (!TextUtils.isEmpty(item)) {
+            mAdapter.add(item);
+            mAdapter.notifyDataSetChanged();
+            mEtItem.setText("");
+        } else {
+            Toast.makeText(this, R.string.empty_item_to_input, Toast.LENGTH_SHORT).show();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @OnItemClick(R.id.listItem)
+    public void selectItemInList(int position) {
+        String item = mAdapter.getItem(position);
+        Toast.makeText(this, "Item Selected: " + item, Toast.LENGTH_SHORT).show();
     }
 }
